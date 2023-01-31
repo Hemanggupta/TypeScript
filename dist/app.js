9b204cc779f057aba6839ada4db38effedff9821
Hemang Gupta <hemang2608@gmail.com>
2023-01-31 16:27:49 +0530

    31-01-2023

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,9 @@
 "use strict";
-const user = "Hemang Gupta";
-console.log(user);
-let Username;
+let Uname = "Hemang";
+{
+    let Uname = "Gupta";
+}
+console.log(Uname);
+const add = (num1, num2) => num1 + num2;
+const sum = (a, b = 5) => console.log(a + b);
+sum(5, 5);
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,12 @@
-const user = "Hemang Gupta";
-console.log(user);
+let Uname = "Hemang";
+{
+	let Uname = "Gupta";
+}
+console.log(Uname);
 
-let Username;
+// Arrow function
+const add = (num1: number, num2: number) => num1 + num2;
+
+const sum: (a: number, b: number) => void = (a, b = 5) => console.log(a + b);
+
+sum(5, 5);
